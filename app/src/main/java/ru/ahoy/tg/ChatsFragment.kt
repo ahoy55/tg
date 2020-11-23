618132f3d191beb3bf3ff8fc51133317b6d65706
ahoy55 <62417883+ahoy55@users.noreply.github.com>
2020-11-23 20:41:20 +0300

    tut 3

---
+++ app/src/main/java/ru/ahoy/tg/ChatsFragment.kt
@@ -0,0 +1,26 @@
+package ru.ahoy.tg
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.ahoy.tg.databinding.FragmentChatsBinding
+
+
+class ChatsFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentChatsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentChatsBinding.inflate(layoutInflater)
+        return mBinding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
